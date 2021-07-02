e5ec2cb4a18301eea75ba7a408b2088831a8ab2a
svqzd <eatpiern@protonmail.com>
2021-07-02 22:10:27 +0300

    fix: moved pull to its own file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 'use strict'
 
 const { prompt } = require('inquirer')
-const { exec, spawnSync, execSync } = require('child_process')
 const { readdirSync } = require('fs')
-const { isWebUri } = require('valid-url')
-const str = require('@supercharge/strings')
+const { exec } = require('child_process')
 const execute = require('./utils/exec/shell')
 const commit = require('./utils/git/commit')
 const push = require('./utils/git/push')
@@ -142,71 +140,7 @@ try {
               })
               break
             case 'pull':
-              prompt([
-                {
-                  type: 'input',
-                  message:
-                    'Please enter the URL of the repository you want to pull.',
-                  name: 'url',
-                },
-              ]).then(({ url }) => {
-                const dir = readdirSync(process.cwd())
-                let confirmed = false
-                let _str
-                // console.log(dir)
-                if (dir.includes('.git')) {
-                  if (isWebUri(url) && url.endsWith('.git')) {
-                    const status = execSync('git status')
-                    if (
-                      status
-                        .toString()
-                        .includes(
-                          'no changes added to commit (use "git add" and/or "git commit -a")'
-                        )
-                    ) {
-                      console.log(
-                        str(status.toString())
-                          .after(
-                            '(use "git restore <file>..." to discard changes in working directory)'
-                          )
-                          .before(
-                            'no changes added to commit (use "git add" and/or "git commit -a")'
-                          )
-                          .get()
-                      )
-                    } else {
-                      console.log(
-                        str(status.toString())
-                          .after(
-                            '(use "git restore <file>..." to discard changes in working directory)'
-                          )
-                          .get()
-                      )
-                    }
-                    prompt([
-                      {
-                        type: 'confirm',
-                        message:
-                          'Are you sure you want to pull this repository?',
-                        name: 'confirmation',
-                      },
-                    ]).then(({ confirmation }) => {
-                      return confirmation ? (confirmed = true) : null
-                    })
-
-                    if (confirmed) {
-                      const pullOutput = execSync('git pull')
-                      console.log(pullOutput)
-                    }
-                  } else {
-                    return console.log('❌️ Invalid URL.')
-                  }
-                } else {
-                  return console.log(
-                    '😶 This directory does not have Git initialized!'
-                  )
-                }
-              })
+              pull()
               break
             default:
               return console.log('🧐 Method not implemented yet.')
